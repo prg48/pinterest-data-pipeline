90fbd5dcc2d39e155437c7cd7d19a3d0158a5ec2
prgrg48 <prabingurung2@gmail.com>
2023-12-31 16:09:46 +0000

    remove batch ingestion

--- batch-ingestion-tf/api-invoke-url.tf
+++
@@ -1,5 +0,0 @@
-# write the api gateway invoke url to a local file
-resource "local_file" "invoke_url" {
-  content = aws_api_gateway_stage.project_api_stage.invoke_url
-  filename = "${path.root}/kafka-invoke-url.txt"
-}
--- batch-ingestion-tf/data.tf
+++
@@ -1,9 +0,0 @@
-# get the bucket data created with s3.tf in ../batch-storage-tf folder
-data "aws_s3_bucket" "storage" {
-  bucket = var.bucket_name
-}
-
-# availability zones
-data "aws_availability_zones" "AZs" {
-  state = "available"
-}
